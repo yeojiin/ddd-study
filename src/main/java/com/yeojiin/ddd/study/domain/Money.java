e0a9261360be5101b051bf6967f0c21fea5f4336
jylim <jylim@12cm.co.kr>
2023-02-08 14:43:03 +0900

    chore(Money): Money @Override equals없으면 주소값으로 에러 -> 해결

--- src/main/java/com/yeojiin/ddd/study/domain/Money.java
+++ src/main/java/com/yeojiin/ddd/study/domain/Money.java
@@ -1,4 +1,7 @@
 package com.yeojiin.ddd.study.domain;
+
+import java.util.Objects;
+
 public class Money {
     private int value;
 
@@ -15,6 +18,16 @@ public class Money {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return value == money.value;
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
